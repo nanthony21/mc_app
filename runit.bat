cbd027a24953ea49a08ce7949f316bb443155fe0
nanthony21 <nickmanthony@hotmail.com>
2018-01-28 00:23:09 -0600

    allow all websocket origins

--- runit.bat
+++ runit.bat
@@ -4,4 +4,4 @@ SET parent=%~dp0
 echo %parent%
 SET anaconda_dir=%userprofile%/Anaconda3
 call %anaconda_dir%/Scripts/activate.bat
-bokeh serve --show --allow-websocket-origin 192.168.0.3:6004 --allow-websocket-origin localhost:6004 --port 6004 %parent% 
+bokeh serve --show --allow-websocket-origin * --port 6004 %parent% 
